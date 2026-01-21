2c3f1953009b66705e7408638fa3a7c9c32da2ad
Bexruz <bexruzpy@gmail.com>
2026-01-21 11:50:36 +0500

    .

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from typing import List, Optional
 import models, database, schemas, crud
+from datetime import date
 
 models.Base.metadata.create_all(bind=database.engine)
 
